9330ae0c35b923e1d304dc447935fa78131a5a81
etanf987 <efong987@gmail.com>
2024-07-02 05:14:13 -0400

    Fix dragging functionality

--- script.js
+++ script.js
@@ -62,6 +62,92 @@ document.getElementById('newTask').addEventListener('click', function () {
     let taskElement = document.createElement('div');
     taskElement.textContent = taskName;
     taskElement.className = 'task-item';
+    taskElement.setAttribute("draggable", "true");
     document.getElementById('taskList').appendChild(taskElement);
-  }
+    taskElement.addEventListener('dragstart', handleDragStart, false);
+    taskElement.addEventListener('dragenter', handleDragEnter, false);
+    taskElement.addEventListener('dragover', handleDragOver, false);
+    taskElement.addEventListener('dragleave', handleDragLeave, false);
+    taskElement.addEventListener('drop', handleDrop, false);
+    taskElement.addEventListener('dragend', handleDragEnd, false);
+    }
+  
+  /*let items = document.querySelectorAll('.task-item');
+  for (var index = 0; index < items.length; index++) {
+    console.log(items[index]);
+    
+  } */
+  
+  
 } );
+
+
+// DRAGGING AND DROPPING
+
+
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+  
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+
+  e.dataTransfer.dropEffect = 'move';
+  
+  return false;
+}
+
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  }
+  
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.style.opacity = '1';
+  let items = document.querySelectorAll('.task-item');
+  items.forEach(function (item) {
+    item.classList.remove('over');
+  });
+}
+
+/*
+let items = document.querySelectorAll('.task-item');
+for (var index = 0; index < items.length; index++) {
+  console.log(items[index]);
+  
+}
+items.forEach(function(item) {
+  item.addEventListener('dragstart', handleDragStart, false);
+  item.addEventListener('dragenter', handleDragEnter, false);
+  item.addEventListener('dragover', handleDragOver, false);
+  item.addEventListener('dragleave', handleDragLeave, false);
+  item.addEventListener('drop', handleDrop, false);
+  item.addEventListener('dragend', handleDragEnd, false);
+});
+*/
+
